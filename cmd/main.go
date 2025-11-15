130c863fcef487db626a78fc7c9280f73ce41874
Sanjay Velu <sanjayvelu147@gmail.com>
2025-11-15 22:26:18 +0530

    intial commit go ping server

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default() // Use Default() for logging and recovery middleware
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	// Run on custom port with error handling
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
+}
---
+++ dockerfile
@@ -0,0 +1,13 @@
+FROM  golang:1.24-alpine AS builder
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN go build -o server ./cmd
+
+
+FROM alpine:latest
+WORKDIR /root/
+COPY --from=builder /app/server .
+EXPOSE 8080
+CMD [ "./server" ]
